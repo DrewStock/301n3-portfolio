36dbb1d744e4c8d43b53f58d840bd735d9d7e756
DrewStock <andrewdstock@gmail.com>
2016-08-28 19:10:54 -0700

    created constructor function for portfolio projects

--- scripts/portfolioBuild.js
+++ scripts/portfolioBuild.js
@@ -1,1 +1,29 @@
-// Script goes here
+
+var projectArticles = [];
+
+function ProjectArticle (prs) {
+  this.projectTitle = prs.projectTitle;
+  this.projectUrl = prs.projectUrl;
+  this.projectDesc = prs.projectDesc;
+};
+
+ProjectArticle.prototype.toHtml = function() {
+  var $newProjectArticle = $('article.projectTemplate').clone();
+
+  $newProjectArticle.find('a.projectLink').attr('href', this.projectUrl);
+  $newProjectArticle.find('h2').html(this.projectTitle);
+  $newProjectArticle.find('article.projectDescription').html(this.projectDesc);
+  // $newProjectArticle.find('.article-body').html(this.body);
+  $newProjectArticle.removeClass('projectTemplate');
+
+  return $newProjectArticle;
+};
+
+
+portfolioData.forEach(function(el) {
+  projectArticles.push(new ProjectArticle(el));
+});
+
+projectArticles.forEach(function(a){
+  $('#projects').append(a.toHtml());
+});
---
+++ scripts/portfolioSource.js
@@ -0,0 +1,12 @@
+var portfolioData = [
+  {
+    projectTitle:   '4TheRodentz',
+    projectUrl:     'https://4-the-rodentz.github.io/App/html/index.html',
+    projectDesc:    '<p>4theRodentz offers a fun Pokemon-Go-esque animal tracking game in order to track the environmental impact of global warming on rodents. The app was inspired by Cascasdes Pika Watch, a program sponsored by the Oregon Zoo and the U.S. Geological Survey. This program crowdsources the collection of data on sightings of pikas, which are small wild rodents that live in the Columbia River Gorge. We wanted to make an app that crowdsources the tracking of common rodents in the Greater Portland Area. Rodent aficionados and beginners alike can enter their data, which includes prompts like the size of the rodent, and explains easily recognizable rodent features.</p>'
+  },
+  {
+    projectTitle:   'Market Analysis app',
+    projectUrl:     'https://drewstock.github.io/market-analysis-app/',
+    projectDesc:    '<p>Market Analysis app is an application that enables users to select images that represent products they would be interested in purchasing. The user data is stored for analysis, with an end goal of determining which products should be manufactured and marketed.</p>'
+  },
+];
